eefeae3997b50c8eae8593ee405141effb461212
wellington <andrewOOP@users.noreply.github.com>
2018-09-10 00:27:57 -0500

    Initial commit

---
+++ Source/UNREAL_TUTORIAL_3/UNREAL_TUTORIAL_3GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UNREAL_TUTORIAL_3GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_TUTORIAL_3_API AUNREAL_TUTORIAL_3GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
